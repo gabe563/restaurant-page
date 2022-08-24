440232bd7f17cccfcdf60eec7609b7a676cdb0b5
FOXYSAMU <reefebghr@gmail.com>
2022-08-24 12:35:28 -0400

    Production mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
@@ -10,7 +10,6 @@ module.exports = {
     clean: true,
     assetModuleFilename: '[name][ext]',
   },
-  devtool: 'source-map',
   module: {
     rules: [
       {
